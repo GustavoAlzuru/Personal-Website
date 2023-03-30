ee7b6b1ad01c3f6bcf299c91fbfcbaa0ce98a528
Gustavo Alzuru <gustavoalzuru7@gmail.com>
2023-03-30 16:48:23 -0400

    updating layout

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const currentTheme = localStorage.getItem('theme')
+const buttonDarkMode = document.getElementById('buttonDarkMode')
 if(currentTheme){
     document.body.classList.add(currentTheme)
+    buttonDarkMode.textContent = '☀'
 }
 
 function darkMode(){
     document.body.classList.toggle('dark-mode')
     if(document.body.classList.contains('dark-mode')){
         localStorage.setItem('theme', 'dark-mode')
+        buttonDarkMode.textContent = '☀'
     }else{
         localStorage.removeItem('theme', 'dark-mode')
+        buttonDarkMode.textContent = '☽'
     }
 }
