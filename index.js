61af6aeae05b63f97c64cfb32e734536e17d0ca8
Gustavo Alzuru <gustavoalzuru7@gmail.com>
2023-04-01 00:43:19 -0400

    update animation scroll

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+// ANIMATE ON SCROLL
+
+const hiddenEl = document.querySelectorAll('.hidden')
+const hiddenLeft = document.querySelector('.hidden-left')
+const hiddenRight = document.querySelector('.hidden-right')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    }))
+})
+
+const observerLeft = new IntersectionObserver((entries) => {
+    entries.forEach((entry => {
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show-img')
+        }else{
+            entry.target.classList.remove('show-img')
+        }
+    }))
+}
+)
+hiddenEl.forEach((element) => {
+    observer.observe(element)
+})
+observerLeft.observe(hiddenLeft)
+observerLeft.observe(hiddenRight)
+// DARK THEME
+
 const currentTheme = localStorage.getItem('theme')
 const buttonDarkMode = document.getElementById('buttonDarkMode')
 if(currentTheme){
@@ -15,3 +50,5 @@ function darkMode(){
         buttonDarkMode.textContent = '☽'
     }
 }
+
+// const hiddenImg = document.querySelector('.about-me-img')
